525bb590da78c520b9b275aaf5a27fb870c5c3ab
Alex Nogueira <alex@devaction.com.br>
2024-09-21 11:17:22 -0300

    feat: add Comment model, factory, migration, and seeder

---
+++ database/migrations/2024_09_21_141447_create_comments_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\{Task, User};
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Task::class)->constrained();
+            $table->foreignId(User::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
